b30e90a6b91e96ab1f7efba5cddc1547036dc71f
Alisa <solomatinaalisa14@gmail.com>
2023-03-23 22:26:08 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 13676;
+        int mile = 20;
+
+        System.out.println("Начислено бонусов:" + priceTicket / mile);
+    }
+}
